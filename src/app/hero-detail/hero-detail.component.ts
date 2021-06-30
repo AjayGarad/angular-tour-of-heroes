ba8d598f13441c1a0495b853d98391b47786a220
Ajay Garad <ajayg@dosepack.com>
2021-06-30 12:48:50 +0530

    update - get data from server using httpclientmodule implemented

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import {Hero} from '../hero';
 import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-hero-detail',
@@ -10,7 +10,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero?: Hero;
+  hero?: Hero;
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
@@ -20,7 +20,12 @@ export class HeroDetailComponent implements OnInit {
   ngOnInit(): void {
     this.getHero();
   }
-  
+
+  getHeroes(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
     this.heroService.getHero(id)
